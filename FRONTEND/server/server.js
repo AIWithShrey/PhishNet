1c98ed7e5b931902a600cf71ee99840cb4bfc68c
JNeshan <133393303+JNeshan@users.noreply.github.com>
2024-04-06 17:19:27 -0500

    Set up express

---
+++ FRONTEND/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '..', '/Phishnet/dist')));
+
+// Define a route for the initial page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/Phishnet/dist/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
